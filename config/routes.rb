eea7941ed4a0e019a963f477b6fc2115813bece4
Wisenk <wisenkmr93@gmail.com>
2025-06-30 16:13:07 +0000

    blog_add activestorage, gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'homes/top'
+root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
