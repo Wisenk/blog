7dcb221dba3359c1c7f929344ec513dbf30566f5
Wisenk <wisenkmr93@gmail.com>
2025-07-06 14:21:19 +0000

    blog_install ImageMagic

--- app/controllers/post_blogs_controller.rb
+++ app/controllers/post_blogs_controller.rb
@@ -1,6 +1,22 @@
 class PostBlogsController < ApplicationController
+
   def new
+    @post_blog = PostBlog.new
   end
+
+  def create
+    #
+    @post_blog = PostBlog.new(post_blog_params)
+    @post_blog.user = current_user.id
+    @post_blog.image.attach(params[:post_blog][:image]) if params[:post_blog][:image].present?
+    if @post_blog.save
+      flash[:notice] = "Post was successfully created."
+      redirect_to post_blog_path(@post_blog) 
+    else
+      flash.now[:alert] = "Failed to create post. Please check the errors."
+      render :new
+    #
+    end
 
   def edit
   end
@@ -13,5 +29,12 @@ class PostBlogsController < ApplicationController
 
   def index
     @post_blogs = PostBlog.all
+  end
+
+
+  private
+
+  def post_blog_params
+    params.require(:post_blog).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'post_blogs/new'
-  get 'post_blogs/edit'
-  get 'post_blogs/show'
-  get 'post_blogs/view'
-  devise_for :users
-root to: "homes#top"
+devise_for :users
+root to: 'homes#top'
 
-  resources :post_blogs, only: [:new, :index, :show, :edit]
+resources :post_blogs, only: [:new, :create, :index, :show, :edit]
+resources :users, only: [:show, :edit, :update]
+
+
   get 'about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
