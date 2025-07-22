8434609fa63bb38c540d0a0ecc604e2e34ae2cb8
Wisenk <wisenkmr93@gmail.com>
2025-07-22 16:52:17 +0000

    blog_add user edit and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
   end
 
@@ -6,10 +7,35 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @post_blogs = @user.post_blogs
+    @user = current_user
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "Profile was successfully updated."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user.destroy
+      flash[:notice] = "Account was successfully deleted."
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image, :introduction)
   end
 end
