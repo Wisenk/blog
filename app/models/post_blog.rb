d6c9b2695934b2837a2d68149be5b3879bdba156
Wisenk <wisenkmr93@gmail.com>
2025-07-24 14:56:14 +0000

    blog_add active text

--- app/models/post_blog.rb
+++ app/models/post_blog.rb
@@ -3,8 +3,17 @@ class PostBlog < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  validates :title, presence: true, length: { minimum: 2, maximum: 100 }
-  validates :body, presence: true, length: { minimum: 5, maximum: 1000 }
-  validates :image, content_type: [:png, :jpg, :jpeg], size: { less_than: 5.megabytes }
+  has_rich_text :body
 
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [500, 500]).processed
+  end
+
+  validates :title, presence: true, length: { minimum: 2, maximum: 100 }
+  #validates :body, presence: true, length: { minimum: 5, maximum: 1000 }
+  validates :image, content_type: [:png, :jpg, :jpeg], size: { less_than: 5.megabytes }
 end
