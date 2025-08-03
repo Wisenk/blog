e705b244f7dde191b927ff0d1106504238efa6a1
Wisenk <wisenkmr93@gmail.com>
2025-08-03 06:21:47 +0000

    blog_change evironments/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts.clear
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -119,4 +119,3 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
 
-config.hosts.clear
