5ac1ef9859981999aa6e5898f3c3e04140d36ae1
Wisenk <wisenkmr93@gmail.com>
2025-08-04 15:45:35 +0000

    blog_add comments until controllers

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,2 +1,18 @@
 class PostCommentsController < ApplicationController
+
+    def create
+        @post_blog = PostBlog.find(params[:post_blog_id])
+        @post_comment = current_user.post_comments.new(post_comment_params)
+        @post_comment.post_blog.id = @post_blog.id
+        # Ensure the post_comment is associated with the post_blog
+        @post_comment.save
+        redirect_to post_blog_path(@post_blog), notice: "Comment was successfully created."
+    end
+
+
+    private
+    def post_comment_params
+        params.require(:post_comment).permit(:content)
+    end
+
 end
