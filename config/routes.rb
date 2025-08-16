e6f7fe2d62a483f1ebf227155dcc80d7ce3981d6
Wisenk <wisenkmr93@gmail.com>
2025-08-16 10:03:29 +0000

    blog_add locales jp time

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  #devise_for :users, controllers: {
+  #  registrations: 'public/registrations',
+  #  sessions: 'public/sessions',
+  #  passwords: 'public/passwords'
+  #}
+
+  #devise_for :admins, path: "admin", skip: [:registrations, :password], controllers: {
+  #  sessions: 'admin/sessions'
+  #}
+
   scope module: :public do
     devise_for :users, controllers: {
       registrations: 'public/registrations'
@@ -8,8 +18,10 @@ Rails.application.routes.draw do
     get '/myposts' => 'post_blogs#myposts', as: 'myposts'
     get 'about' => 'homes#about', as: 'about'
     
+  
     resources :post_blogs, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
       resources :post_comments, only: [:create, :destroy]
+      resources :favorites, only: [:create, :destroy]
       
       collection do
         get 'search'
@@ -18,16 +30,24 @@ Rails.application.routes.draw do
 
     resources :users do
       resources :post_blogs, only: [:index, :show]
+      #member do
+      #  get :post_blogs
+      #  get :favorites
+      #  get :followings
+      #  get :followers
+      #end
     end
   end
 
 
 
-devise_for :admin, skip: [:registrations, :password], controllers: {
-  sessions: 'admin/sessions'
-}
+
 
 namespace :admin do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
   resources :users, only: [:index, :destroy]
   resources :post_blogs, only: [:index, :destroy]
   resources :post_comments, only: [:index, :destroy]
