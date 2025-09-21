2e3032cdeb094ccc1bb6ea629021cd888585f13f
Ubuntu <ubuntu@ip-172-31-46-220.ap-northeast-1.compute.internal>
2025-09-21 11:24:04 +0000

    blog_change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts.clear
+
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
